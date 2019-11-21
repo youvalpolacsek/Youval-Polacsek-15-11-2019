51e698c945d7ed1f6a6321ae3ebde24d083dbcd6
Youval Polacsek <Youval@h-MacBook-Pro-sl-Youval.local>
2019-11-21 15:16:16 +0200

    Updates

---
+++ precache-manifest.28364a1454fff92f21eed4723041c28f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6240d0384b03c94eeff91be965ce93",
+    "url": "/youvalpolacsek/Youval-Polacsek-15-11-2019/index.html"
+  },
+  {
+    "revision": "ac18f6cf2b4f135a53bc",
+    "url": "/youvalpolacsek/Youval-Polacsek-15-11-2019/static/css/main.98822ef1.chunk.css"
+  },
+  {
+    "revision": "049dc08504dc748e8bc2",
+    "url": "/youvalpolacsek/Youval-Polacsek-15-11-2019/static/js/2.48d3e2e9.chunk.js"
+  },
+  {
+    "revision": "ac18f6cf2b4f135a53bc",
+    "url": "/youvalpolacsek/Youval-Polacsek-15-11-2019/static/js/main.12a19db2.chunk.js"
+  },
+  {
+    "revision": "99a551a2f9fddad417b6",
+    "url": "/youvalpolacsek/Youval-Polacsek-15-11-2019/static/js/runtime-main.bdd7a125.js"
+  }
+]);
